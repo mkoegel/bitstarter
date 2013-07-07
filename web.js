1534e25b89c762693131f4ed9361d08e9d9a25b3
Michael Koegel <michael@koegel.org>
2013-07-07 09:22:15 +0200

    Changed web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	fs.readFile('index.html', function (err, data) {
+	  if (err) throw err;
+	  console.log(data);
+  	  response.send(data.toString());
+	});
 });
 
 var port = process.env.PORT || 5000;
